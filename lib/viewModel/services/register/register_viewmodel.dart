03bdc6796b7da43ece0c6c16cc6841a92573edb4
ahammed hinas <82311707+hinasahammed@users.noreply.github.com>
2024-08-09 10:39:31 +0530

    Update register_viewmodel.dart

--- lib/viewModel/services/register/register_viewmodel.dart
+++ lib/viewModel/services/register/register_viewmodel.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:invoice_ui/res/utils/utils.dart';
+import 'package:invoice_ui/view/login/login_view.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class RegisterViewmodel {
-  void register(
-      String username, String email, String contact, String password) async {
+  void register(String username, String email, String contact, String password,
+      BuildContext context) async {
     final pref = await SharedPreferences.getInstance();
     await pref.setString("userName", username);
     await pref.setString("email", email);
     await pref.setString("contact", contact);
-    // await pref.setString("password", password).then((value) {
-    //   Navigator.push(context, route)
-    // },);
+    await pref.setString("password", password).then(
+      (value) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const LoginView(),
+            )).then(
+          (value) => Utils().showFlushToast(
+            context,
+            "Success",
+            "Account created sucessfully, try to login",
+          ),
+        );
+      },
+    );
   }
 }
