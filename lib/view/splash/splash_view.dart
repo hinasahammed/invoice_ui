f9f8d57b1e1e84178d6519a91508e40c6bb20ffc
ahammed hinas <82311707+hinasahammed@users.noreply.github.com>
2024-08-09 10:02:20 +0530

    updated splashviemodel on splash view

--- lib/view/splash/splash_view.dart
+++ lib/view/splash/splash_view.dart
@@ -2,10 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:invoice_ui/view/login/login_view.dart';
-import 'package:invoice_ui/view/tabBar/custom_tab_bar_view.dart';
+import 'package:invoice_ui/viewModel/services/splash/splash_viewmodel.dart';
 import 'package:lottie/lottie.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -20,38 +18,11 @@ class _SplashViewState extends State<SplashView> {
     super.initState();
     Timer(
       const Duration(seconds: 3),
-      () => isLogin(),
+      () => SplashViewmodel().isLogin(context),
     );
   }
 
-  void isLogin() async {
-    final pref = await SharedPreferences.getInstance();
-    var val = pref.getBool("isLogedin");
-    if (val != null) {
-      if (val) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const CustomTabBarView(),
-          ),
-        );
-      } else {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const LoginView(),
-          ),
-        );
-      }
-    } else {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const LoginView(),
-        ),
-      );
-    }
-  }
+  
 
   @override
   Widget build(BuildContext context) {
