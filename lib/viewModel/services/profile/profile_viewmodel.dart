b576db4306f7c080f65e068c83492becd4e5c9b5
ahammed hinas <82311707+hinasahammed@users.noreply.github.com>
2024-08-12 11:05:29 +0530

    Update profile_viewmodel.dart

--- lib/viewModel/services/profile/profile_viewmodel.dart
+++ lib/viewModel/services/profile/profile_viewmodel.dart
@@ -1,11 +1,10 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:invoice_ui/res/utils/theme.dart';
 import 'package:invoice_ui/view/login/login_view.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileViewmodel {
-
-  
   void logout(BuildContext context) async {
     final pref = await SharedPreferences.getInstance();
     pref.setBool("isLogedin", false).then(
@@ -17,7 +16,9 @@ class ProfileViewmodel {
         );
   }
 
-  void logoutDialogue(BuildContext context) {
+  void logoutDialogue(
+    BuildContext context,
+  ) {
     AwesomeDialog(
       context: context,
       animType: AnimType.bottomSlide,
@@ -33,5 +34,17 @@ class ProfileViewmodel {
       desc: "Are you want to logout?",
       title: "Logout",
     ).show();
+  }
+
+  void getTheme() async {
+    final pref = await SharedPreferences.getInstance();
+    var val = pref.getBool("isDarkmode") ?? true;
+    MyThemes.isDark.value = val;
+  }
+
+  void setTheme(bool value) async {
+    final pref = await SharedPreferences.getInstance();
+
+    pref.setBool("isDarkmode", value);
   }
 }
